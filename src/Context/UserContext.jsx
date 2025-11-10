f6be48bb189062c6d68e70682a3ed73eb25404ed
Precisy004 <ayomideadesanya37@gmail.com>
2025-11-10 03:31:40 +0100

    Fix merge conflicts and clean App.jsx, add Navbar and Footer

---
+++ src/Context/UserContext.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { UserContext } from './UserContextValue';
+
+export const UserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const login = (user) => setCurrentUser(user);
+  const logout = () => setCurrentUser(null);
+
+  return (
+    <UserContext.Provider value={{ currentUser, login, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
